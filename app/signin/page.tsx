50f69ebd4cf0b00be985945e42a2e5e6b8c0e215
AnisKehila <aniskehila22@gmail.com>
2023-07-17 14:52:05 +0100

    auth with firebase

---
+++ app/signin/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { useState, FormEvent } from "react";
+import signIn from "../firebase/auth/signIn";
+import { useRouter } from "next/navigation";
+
+function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleForm = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const { result, error } = await signIn(email, password);
+
+    if (error) {
+      return console.log(error);
+    }
+    console.log(result);
+    return router.push("/");
+  };
+  return (
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1 className="mt-60 mb-30">Login</h1>
+        <form onSubmit={handleForm} className="form">
+          <label htmlFor="email">
+            <p>Email</p>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@mail.com"
+            />
+          </label>
+          <label htmlFor="password">
+            <p>Password</p>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+            />
+          </label>
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
