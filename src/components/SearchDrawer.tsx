3055a0ea02af2ddbc0905515b52b14a7e1e7c13b
aniskehila <aniskehila22@gmail.com>
2023-09-28 14:07:15 +0100

    add search functionality

--- src/components/SearchDrawer.tsx
+++ src/components/SearchDrawer.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useRef, useState } from "react";
 
 import SearchInput from "./ui/SearchInput";
+import { UserData } from "@/types";
 
 const SearchDrawer = ({
   isActive,
@@ -12,33 +13,45 @@ const SearchDrawer = ({
 }) => {
   const drawerRef = useRef<HTMLDivElement>(null);
   const [search, setSearch] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState<UserData[]>([]);
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      drawerRef.current &&
+      !drawerRef.current.contains(event.target as Node) &&
+      isActive
+    ) {
+      setIsActive(false);
+    }
+  };
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        drawerRef.current &&
-        !drawerRef.current.contains(event.target as Node) &&
-        isActive
-      ) {
-        setIsActive(false);
-      }
-    };
     document.addEventListener("click", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);
     };
-  }, [setIsActive, isActive]);
+  });
   return (
     <div
-      className={`fixed z-10 h-screen shadow-[4px_0_24px_rgba(0,0,0,.15)] bg-white top-0 transition-all overflow-hidden duration-300 w-0 ${
-        isActive && "w-[397px] left-[4.6rem] z-10  px-4 py-5"
+      className={`fixed z-10 h-screen shadow-[4px_0_24px_rgba(0,0,0,.15)] dark:bg-[#000000]  bg-white top-0 transition-all overflow-hidden duration-300 w-0 ${
+        isActive &&
+        "w-[397px] left-[4.6rem] dark:border-r border-[#262626] px-4 py-5"
       } ${!isActive && "left-10"}`}
       ref={drawerRef}
     >
       <h2 className="font-[600] text-[24px]">Search</h2>
       <div className="mt-8">
-        <SearchInput search={search} setSearch={setSearch} />
+        <SearchInput
+          setIsLoading={setIsLoading}
+          searchValue={search}
+          setSearch={setSearch}
+          setResults={setResults}
+        />
       </div>
-      <div className="mt-8"></div>
+      <div className="mt-8">
+        {results.map((result) => (
+          <div key={result.userId}>{result.fullName}</div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -1,35 +1,69 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Search from "@/assets/icons/Search.svg";
+import { UserData } from "@/types";
+import search from "@/firebase/search";
 
 const SearchInput = ({
-  search,
+  searchValue,
   setSearch,
+  setResults,
+  setIsLoading,
 }: {
-  search: string;
+  searchValue: string;
   setSearch: React.Dispatch<React.SetStateAction<string>>;
+  setResults: React.Dispatch<React.SetStateAction<UserData[]>>;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(
+    null,
+  );
+
+  useEffect(() => {
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+    const timeoutId = setTimeout(() => {
+      performSearch();
+    }, 500);
+    setTypingTimeout(timeoutId);
+    return () => {
+      if (typingTimeout) {
+        clearTimeout(typingTimeout);
+      }
+    };
+  }, [searchValue]);
+
+  const performSearch = async () => {
+    if (!searchValue) {
+      setResults([]);
+      return;
+    }
+    setIsLoading(true);
+    const results = await search({ search: searchValue });
+    setResults(results);
+    setIsLoading(false);
+  };
+
   return (
-    <label
-      htmlFor="search"
-      className="group relative flex w-full bg-[#EEEE] px-4 py-2 rounded-md cursor-text items-center gap-2"
-    >
-      <Search className="fill-gray-400 group-focus-within:hidden " />
+    <div className="group flex w-full bg-[#EEEE] dark:bg-black px-4 py-2 rounded-md cursor-text items-center gap-2">
+      <Search className="fill-gray-400 group-focus-within:hidden" />
       <input
         type="text"
         name="search"
         id="search"
-        value={search}
+        value={searchValue}
         onChange={(e) => setSearch(e.target.value)}
         placeholder="Search"
         className="outline-none bg-transparent w-full"
       />
       <span
-        className="absolute z-0 right-2 rotate-45 text-lg bg-gray-300 text-[#EEEE] p-2 w-2 h-2 opacity-0 group-focus-within:opacity-100 flex items-center justify-center rounded-[50%] cursor-pointer select-none"
+        className=" rotate-45 text-lg bg-gray-300 dark:text-black text-[#EEEE] p-2 w-2 h-2 opacity-0 group-focus-within:opacity-100 flex items-center justify-center rounded-[50%] cursor-pointer select-none"
         onClick={() => setSearch("")}
       >
         +
       </span>
-    </label>
+    </div>
   );
 };
 
