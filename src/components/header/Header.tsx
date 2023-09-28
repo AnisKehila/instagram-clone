e4d8a5b3dd79d889c67b9c60de18fe7d1ffd43fc
aniskehila <aniskehila22@gmail.com>
2023-09-28 17:42:00 +0100

    fix search on sm screens

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,9 +4,12 @@ import LogoBlack from "@/assets/icons/LogoBlack.svg";
 import Heart from "@/assets/icons/ActivityFeed.svg";
 import Link from "next/link";
 import SearchInput from "../ui/SearchInput";
+import { UserData } from "@/types";
 
 const Header = () => {
   const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState<UserData[]>([]);
   return (
     <header className="sm:hidden px-4 sticky top-0 bg-white border-b shadow-sm z-30 w-full">
       <div className="flex justify-between items-center py-3">
@@ -17,7 +20,12 @@ const Header = () => {
           <LogoBlack />
         </Link>
         <div className="flex items-center gap-5">
-          <SearchInput search={search} setSearch={setSearch} />
+          <SearchInput
+            searchValue={search}
+            setSearch={setSearch}
+            setIsLoading={setIsLoading}
+            setResults={setResults}
+          />
           <Link
             href="/notifications"
             className="transition duration-100 scale-110 hover:scale-[1.2] active:scale-100 active:opacity-60 "
