3e825e659661bb301391191c36f6d4cb302ee80a
AnisKehila <aniskehila22@gmail.com>
2023-11-08 21:26:36 +0100

    add new message icon to messages page

--- src/components/conversation-list/ConversationList.tsx
+++ src/components/conversation-list/ConversationList.tsx
@@ -1,14 +1,21 @@
 "use client";
 import { useAuthContext } from "@/contexts/AuthContext";
-import React from "react";
-
+import React, { useState } from "react";
+import NoteIcon from "@/assets/icons/Note.svg";
+import SendMessageModal from "../modals/SendMessageModal";
 const ConversationList = () => {
   const { userData } = useAuthContext();
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <div className="flex">
-        <div className="font-bold">{userData?.userName}</div>
+    <div className="border-r border-[#A8A8A8] min-h-screen">
+      <div className="flex px-7 py-8 w-full justify-between">
+        <span className="font-bold text-xl">{userData?.userName}</span>
+        <span onClick={() => setIsOpen(true)} className="cursor-pointer">
+          <NoteIcon />
+        </span>
       </div>
+      <span className="px-7 font-bold text-[17px]">Messages</span>
+      <SendMessageModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
   );
 };
