d7f0d38df8ffbb39bb8d1915b026e216611a419c
AnisKehila <aniskehila22@gmail.com>
2023-11-11 23:22:06 +0100

    search for users to start a chat

--- src/components/modals/SendMessageModal.tsx
+++ src/components/modals/SendMessageModal.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../ui/Modal";
+import Exit from "@/assets/icons/Exit.svg";
+import SearchInput from "../ui/SearchInput";
+import { UserData } from "@/types";
 
 const SendMessageModal = ({
   isOpen,
@@ -8,9 +11,30 @@ const SendMessageModal = ({
   isOpen: boolean;
   setIsOpen: (arg: boolean) => void;
 }) => {
+  const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState([{} as UserData]);
   return (
-    <Modal open={isOpen} setOpen={setIsOpen}>
-      Test
+    <Modal
+      open={isOpen}
+      setOpen={setIsOpen}
+      className="w-full mx-2 md:w-[548px] "
+    >
+      <div className="flex items-center border-b p-4">
+        <span className="w-full text-center text-lg font-semibold">
+          New message
+        </span>
+        <Exit className="cursor-pointer" onClick={() => setIsOpen(false)} />
+      </div>
+      <div className="flex items-center p-3">
+        <span className="text-lg font-semibold">To: </span>
+        <SearchInput
+          searchValue={search}
+          setSearch={setSearch}
+          setIsLoading={setIsLoading}
+          setResults={setResults}
+        />
+      </div>
     </Modal>
   );
 };
